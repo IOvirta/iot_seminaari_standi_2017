d2a0e924f8d92d28d6611515a4c8c18be981efe1
bjuutro <pietari.noeskoski@eng.tamk.fi>
2017-02-06 20:31:33 +0200

    encoded_frame ro5

--- inc/video/encoded_frame.hpp
+++ inc/video/encoded_frame.hpp
@@ -1,13 +1,53 @@
 #ifndef IOVIRTA_IOT_VIDEO_ENCODER_ENCODED_FRAME_HPP
 #define IOVIRTA_IOT_VIDEO_ENCODER_ENCODED_FRAME_HPP
 
+#include <cstdint>
+#include <cstddef>
+#include <cstring>
+
+#include <algorithm>
+
 namespace iovirta_iot
 {
 namespace video
 {
 struct EncodedFrame
 {
+    std::uint8_t *data = nullptr;
+    std::size_t size = 0;
+    /*video size, koodaus*/
 
+    EncodedFrame() {}
+    ~EncodedFrame()
+    {
+        if (data != nullptr)
+            delete[] data;
+    }
+    EncodedFrame(const EncodedFrame &other)
+    {
+        if (other.data != nullptr)
+        {
+            size = other.size;
+            data = new std::uint8_t[size];
+
+            std::memcpy(data, other.data, size);
+        }
+    }
+    EncodedFrame(EncodedFrame &&other) : data(other.data), size(other.size)
+    {
+        other.size = 0;
+        other.data = nullptr;
+    }
+    EncodedFrame& operator=(EncodedFrame other)
+    {
+        if (this != &other)
+        {
+            std::swap(data, other.data);
+            std::swap(size, other.size);
+        }
+
+        return *this;
+    }
 }; // struct EncodedFrame
 } // namespace video
 } // namespace iovirta_iot
